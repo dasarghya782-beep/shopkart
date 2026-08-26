1df4c484675d733e18bb54373718a2d33f1c799d
Arghya Deep Dash <dasarghya782@gmail.com>
2026-08-26 16:34:36 +0530

    Remove unused DuplicateEmailException (not yet wired in)

--- backend/src/main/java/com/shopkart/exception/DuplicateEmailException.java
+++
@@ -1,8 +0,0 @@
-package com.shopkart.exception;
-
-public class DuplicateEmailException extends RuntimeException{
-
-    public DuplicateEmailException(String email){
-        super("Customer with email: " + email + " already exist");
-    }
-}
